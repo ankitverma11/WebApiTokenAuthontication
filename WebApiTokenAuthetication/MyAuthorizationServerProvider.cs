bfa9b6554f93f2e4b07c4916ac64b0e915b6a409
Ankit Verma <anki.verma02@hotmail.com>
2019-01-08 19:22:37 +0530

    WebApiTokenAuthetication

---
+++ WebApiTokenAuthetication/MyAuthorizationServerProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApiTokenAuthetication
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); //method used for validated the client
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);  // method we will validate creditial user if creditial is fine generate token
+            if (context.UserName.Equals("admin") && context.Password.Equals("admin"))
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Ankit"));
+                context.Validated(identity);
+            }
+            if (context.UserName.Equals("user") && context.Password.Equals("user"))
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Ankur"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid grant", "username and password is not valid");
+                return;
+            }
+        }
+
+    }
+}
